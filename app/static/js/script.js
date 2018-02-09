99c0235390996ab1d28b64140722e1beaa6bf72c
Stef van Dijk <stef@re-beat.nl>
2018-02-09 14:29:03 +0100

    fix indents

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -18,7 +18,7 @@
      * Toggeling should happen on every click 
      */
     init: function(){
-       if (window.location.hash === '') {
+      if (window.location.hash === '') {
         window.location.hash = '#start';
       } else {
         sections.toggle(window.location.hash);
@@ -43,28 +43,28 @@
     * @memberof sections
     * @param {String} id The ID of the element to enable
     */
-   toggle: function(id) {
-     this.hideElements('section');
-     this.showElement(id);
-   },
+    toggle: function(id) {
+      this.hideElements('section');
+      this.showElement(id);
+    },
 
-   /**
-    * Hide the elements by applying the hidden class.
-    * @param {String} selector The CSS Selector of the elements ot hide
-    */
-   hideElements: function(selector) {
-     document.querySelectorAll(selector).forEach(function(element) {
-       element.classList.add('hidden');
-     });
-   },
+  /**
+  * Hide the elements by applying the hidden class.
+  * @param {String} selector The CSS Selector of the elements ot hide
+  */
+  hideElements: function(selector) {
+    document.querySelectorAll(selector).forEach(function(element) {
+      element.classList.add('hidden');
+    });
+  },
 
    /**
     * Show the element by removing the hidden class
     * @param {String} selector The CSS Selector of the element to show
     */
-   showElement: function(selector) {
-     document.querySelector(selector).classList.remove('hidden');
-   }
+    showElement: function(selector) {
+      document.querySelector(selector).classList.remove('hidden');
+    }
  };
 
   // method that starts the application
